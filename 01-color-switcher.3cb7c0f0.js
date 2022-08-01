6cd358dd793c6d72212ced4f827e0993c1f13cda
Yuliia-Harkusha <97845497+Yuliia-Harkusha@users.noreply.github.com>
2022-08-01 11:26:15 +0000

    Deploying to gh-pages from @ Yuliia-Harkusha/goit-js-hw-09@a8f6ed66490a888f269bc30aa03b55f3d41061b8 🚀

--- 01-color-switcher.3cb7c0f0.js
+++
@@ -1,2 +0,0 @@
-const t={startBtn:document.querySelector("button[data-start]"),stopBtn:document.querySelector("button[data-stop]"),body:document.querySelector("body")};t.stopBtn.disabled=!0;t.startBtn.addEventListener("click",(()=>{t.startBtn.disabled=!0,t.stopBtn.disabled=!1;const e=setInterval((()=>{t.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}),1e3);t.stopBtn.addEventListener("click",(()=>{clearInterval(e),t.startBtn.disabled=!1,t.stopBtn.disabled=!0}))}));
-//# sourceMappingURL=01-color-switcher.3cb7c0f0.js.map
