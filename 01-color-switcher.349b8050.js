a508860617669c776f438686911e76358d3013d8
Yuliia-Harkusha <97845497+Yuliia-Harkusha@users.noreply.github.com>
2022-08-01 11:16:26 +0000

    Deploying to gh-pages from @ Yuliia-Harkusha/goit-js-hw-09@a7631db0e697d87241aa04811840e1ea4f699d39 🚀

---
+++ 01-color-switcher.349b8050.js
@@ -0,0 +1,2 @@
+!function(){var t={startBtn:document.querySelector("button[data-start]"),stopBtn:document.querySelector("button[data-stop]"),body:document.querySelector("body")};t.stopBtn.disabled=!0;t.startBtn.addEventListener("click",(function(){t.startBtn.disabled=!0,t.stopBtn.disabled=!1;var n=setInterval((function(){t.body.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16))}),1e3);t.stopBtn.addEventListener("click",(function(){clearInterval(n),t.startBtn.disabled=!1,t.stopBtn.disabled=!0}))}))}();
+//# sourceMappingURL=01-color-switcher.349b8050.js.map
